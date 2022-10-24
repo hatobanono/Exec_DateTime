f7b5b1b0721fa05af2bc7ff9c367ff0ae6ed518d
hatobanono <chris00iq@gmail.com>
2022-10-24 13:06:23 +0800

    complete

--- Q05/Program.cs
+++ Q05/Program.cs
@@ -13,31 +13,45 @@ namespace Q05
 			Console.Write("請指定年份: ");
 			string input = Console.ReadLine();
 
-			DateTime firstDayOfYear = new DateTime(Convert.ToInt32(input), 1, 1);
-			//Console.WriteLine(firstDayOfYear);
+			int year = Convert.ToInt32(input);
 
-			DayOfWeek dayOfFirstDay = firstDayOfYear.DayOfWeek;
-			DateTime firstSunday = firstDayOfYear;
-			if ( (int)dayOfFirstDay > 0)
+			DateTime firstDay = new DateTime(year, 1, 1);
+
+			int weekday = (int)firstDay.DayOfWeek;
+			DateTime firstSunday = firstDay.AddDays((7 - weekday) % 7);
+
+			DateTime lastDay = new DateTime(year, 12, 31);
+			for (DateTime i = firstSunday; i <= lastDay; i = i.AddDays(7))
 			{
-				firstSunday = firstDayOfYear.AddDays(7 - (int)dayOfFirstDay);
+				Console.WriteLine(i.ToString("yyyy/MM/dd"));
 			}
-			//Console.WriteLine($"第一個禮拜天{firstSunday}");
 
-			DateTime lastDayOfYear = new DateTime(Convert.ToInt32(input), 12, 31);
-			//Console.WriteLine(lastDayOfYear);
 
-			DayOfWeek dayOfLastDay = lastDayOfYear.DayOfWeek;
-			DateTime lastSunday = lastDayOfYear.AddDays(-(int)dayOfLastDay);
-			//Console.WriteLine($"最後一個禮拜天{lastSunday}");
+			//DateTime firstDayOfYear = new DateTime(Convert.ToInt32(input), 1, 1);
+			////Console.WriteLine(firstDayOfYear);
 
-			TimeSpan span = lastSunday - firstSunday;
-			int days = span.Days;
-			for (int i = 0; i <= days; i += 7)
-			{
-				DateTime result = firstSunday.AddDays(i);
-				Console.WriteLine($"{result:yyyy/MM/dd}");
-			}
+			//DayOfWeek dayOfFirstDay = firstDayOfYear.DayOfWeek;
+			//DateTime firstSunday = firstDayOfYear;
+			//if ( (int)dayOfFirstDay > 0)
+			//{
+			//	firstSunday = firstDayOfYear.AddDays(7 - (int)dayOfFirstDay);
+			//}
+			////Console.WriteLine($"第一個禮拜天{firstSunday}");
+
+			//DateTime lastDayOfYear = new DateTime(Convert.ToInt32(input), 12, 31);
+			////Console.WriteLine(lastDayOfYear);
+
+			//DayOfWeek dayOfLastDay = lastDayOfYear.DayOfWeek;
+			//DateTime lastSunday = lastDayOfYear.AddDays(-(int)dayOfLastDay);
+			////Console.WriteLine($"最後一個禮拜天{lastSunday}");
+
+			//TimeSpan span = lastSunday - firstSunday;
+			//int days = span.Days;
+			//for (int i = 0; i <= days; i += 7)
+			//{
+			//	DateTime result = firstSunday.AddDays(i);
+			//	Console.WriteLine($"{result:yyyy/MM/dd}");
+			//}
 		}
 	}
 }
